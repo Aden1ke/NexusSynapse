2562a24b161369da571a0769995851da63ce2691
Aden1ke <sarahogunlalu@gmail.com>
2026-03-15 00:37:33 +0100

    debugging connection issues

--- agents/coder/__init__.py
+++
@@ -1,3 +0,0 @@
-from agents.coder.agent import BuilderAgent, on_agent_event, create_a2a_server
-
-__all__ = ["BuilderAgent", "on_agent_event", "create_a2a_server"]
---
+++ check_env.py
@@ -0,0 +1,1 @@
+﻿import os; from dotenv import load_dotenv; load_dotenv(); print('PROJECT_CONNECTION_STRING:', os.environ.get('PROJECT_CONNECTION_STRING'))
